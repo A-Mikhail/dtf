e83211a7dfcae7b5b88298f47f7d1302004cdfd5
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-12 22:56:01 +0600

    login form

--- application/views/dtf/login.blade.php
+++ application/views/dtf/login.blade.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html> <html> <head>
-<meta charset='utf-8'>
-<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<!DOCTYPE html> <html> <head> <meta charset='utf-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <title>DTF.KZ</title>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 
@@ -10,45 +8,32 @@
 </head>
 
 <body>
-    <form action="/login" method="POST"> {{Form::token()}} <div
-            class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company">
-                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Вход на сайт
-                </h2>
-            </div>
+    <form action="/login" method="POST"> 
+        {{Form::token()}} 
 
-            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form class="space-y-6" action="#" method="POST">
-                    <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        </div>
-                    </div>
+        <main class="form-signin w-100 m-auto">
+            <form>
+                <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+                <h1 class="h3 mb-3 fw-normal">Пожалуйста, авторизуйтесь</h1>
 
-                    <div class="py-4">
-                        <div class="flex items-center justify-between">
-                            <label for="password"
-                                class="block text-sm font-medium leading-6 text-gray-900">Пароль</label>
-                        </div>
+                <div class="form-floating">
+                    <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email адрес</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Пароль</label>
+                </div>
 
-                        <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password"
-                                required
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        </div>
-                    </div>
-
-                    <div class="py-4">
-                        <button type="submit"
-                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Вход</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+                <div class="form-check text-start my-3">
+                    <input class="form-check-input" type="checkbox" value="remember" name='remember' id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Запомнить меня
+                    </label>
+                </div>
+                <button class="btn btn-primary w-100 py-2" type="submit">Вход</button>
+            </form>
+        </main>
     </form>
 
     <script src='/libs/jquery/jquery-3.7.1.min.js'></script>
