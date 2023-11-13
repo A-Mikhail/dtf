54bc2a2572c7eebd673f403f77ee08ee408bed8b
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-13 23:28:15 +0600

    section content

--- application/views/dtf/kanban.blade.php
+++ application/views/dtf/kanban.blade.php
@@ -1,3 +1,6 @@
 @layout('layout')
 
+
+@section('content')
 <h1>Hello world!</h1>
+@endsection('content')
--- application/views/layout.blade.php
+++ application/views/layout.blade.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html> <html> <head>
-<meta charset='utf-8'>
-<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-<title>DTF.KZ</title>
-<meta name='viewport' content='width=device-width, initial-scale=1'>
+<!DOCTYPE html>
+<html>
 
-<link rel='stylesheet' type='text/css' media='screen' href='/libs/jkanban/jkanban.min.css'>
-<link rel='stylesheet' type='text/css' media='screen' href='/libs/bootstrap/css/bootstrap.min.css'>
-<link rel='stylesheet' type='text/css' media='screen' href='/main.css'>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>DTF.KZ</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link rel='stylesheet' type='text/css' media='screen' href='/libs/jkanban/jkanban.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='/libs/bootstrap/css/bootstrap.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='/main.css'>
 </head>
 
 <body>
