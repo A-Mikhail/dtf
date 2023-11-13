3d140a138ce4b146440bfc2dc3e6799a41560d39
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-13 23:32:11 +0600

    UI changes

--- application/views/layout.blade.php
+++ application/views/layout.blade.php
@@ -23,13 +23,8 @@
                 </a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" class="nav-link px-2 text-secondary">Главная</a></li>
+                    <li><a href="#" class="nav-link px-2 text-secondary active">Главная</a></li>
                 </ul>
-
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                    <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..."
-                        aria-label="Search">
-                </form>
 
                 <div class="text-end">
                     <a href="/logout" type="button" class="btn btn-danger">Выход</a>
