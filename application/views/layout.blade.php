cac35430225b3332f198a4d225e79f32c5a10789
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-13 23:31:35 +0600

    header

--- application/views/layout.blade.php
+++ application/views/layout.blade.php
@@ -13,14 +13,30 @@
 </head>
 
 <body>
-    <div class="container">
-        <header class="d-flex justify-content-center py-3">
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Главная</a></li>
-                <a href="/logout" type="button" class="btn btn-danger">Выход</a>
-            </ul>
-        </header>
-    </div>
+    <header class="p-3 text-bg-dark">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                        <use xlink:href="#bootstrap"></use>
+                    </svg>
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-secondary">Главная</a></li>
+                </ul>
+
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                    <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..."
+                        aria-label="Search">
+                </form>
+
+                <div class="text-end">
+                    <a href="/logout" type="button" class="btn btn-danger">Выход</a>
+                </div>
+            </div>
+        </div>
+    </header>
 
     @yield('content')
 
