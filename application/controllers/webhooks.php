46ffbbe0ee720d588aa0df61becd383c82b99876
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-20 21:44:17 +0600

    put without decode

--- application/controllers/webhooks.php
+++ application/controllers/webhooks.php
@@ -5,7 +5,6 @@ class Webhooks_Controller extends Base_Controller {
 
     public function post_messages() {
         $payload=file_get_contents("php://input");
-        $payload=json_decode($payload,true);
 		
 		file_put_contents(path('public').'messages.txt', $payload);
 
