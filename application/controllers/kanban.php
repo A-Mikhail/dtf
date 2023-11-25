b0ad87f3c342c3427fa0d13cb6ddd54d671b2bd2
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-25 23:28:24 +0600

    distinct statuses

--- application/controllers/kanban.php
+++ application/controllers/kanban.php
@@ -4,8 +4,10 @@ class Kanban_Controller extends Base_Controller {
     public $restful = true;
 
     public function get_index() {
-        $clients = Client::where("current_status","!=","reject")->get('name', 'chat_id', 'current_status', 'updated_at');
-        $statuses = Client::where('current_status','!=','reject')->get('current_status');
+        $exclude_statuses = array('reject', 'success');
+
+        $clients = Client::where_not_in('current_status', $exclude_statuses)->get('name', 'chat_id', 'current_status', 'updated_at');
+        $statuses = Client::where_not_in('current_status', $exclude_statuses)->distinct()->get('current_status');
 
         return View::make("dtf.kanban")
             ->with('clients', $clients)
