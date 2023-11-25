5521b26fb9a3ebd8a112fb7cd79dc3f01876687a
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-25 23:17:38 +0600

    kanban statuses generate

--- application/controllers/kanban.php
+++ application/controllers/kanban.php
@@ -4,6 +4,11 @@ class Kanban_Controller extends Base_Controller {
     public $restful = true;
 
     public function get_index() {
-        return View::make("dtf.kanban");
+        $clients = Client::where("current_status","!=","reject")->get('name', 'chat_id', 'current_status', 'updated_at');
+        $statuses = Client::where('current_status','!=','reject')->get('current_status');
+
+        return View::make("dtf.kanban")
+            ->with('clients', $clients)
+            ->with('statuses', $statuses);
     }
 }
--- application/views/dtf/kanban.blade.php
+++ application/views/dtf/kanban.blade.php
@@ -12,5 +12,32 @@
 @endsection
 
 @section('js')
-<script src="/js/kanban.js"></script>
+<script>
+    new jKanban({
+        element: '#kanban',
+        gutter: '15px',
+        widthBoard: '100%',
+        responsivePercentage: true,
+        click: function (el) {
+            alert(el.innerHTML);
+        },
+
+        boards: [
+            @foreach($statuses as $s)
+            {
+                'id': '{{$s->current_status}}',
+                'title': '{{__("statuses.{{$s->current_status}}")}}',
+                'class': 'bg-primary,text-white',
+                'item': [
+                    @foreach($clients as $c)
+                    {
+                        'title': '$c->name'
+                    },
+                    @endforeach
+                ]
+            },
+            @endforeach
+        ]
+    });    
+</script>
 @endsection
