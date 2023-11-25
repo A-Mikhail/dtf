8fcd4c4bd4b2b6a38e9e0c7be5eebc67a34dcd41
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-25 23:37:29 +0600

    add chat_id

--- application/views/dtf/kanban.blade.php
+++ application/views/dtf/kanban.blade.php
@@ -31,7 +31,7 @@
                 'item': [
                     @foreach($clients as $c)
                     {
-                        'title': '{{$c->name}}'
+                        'title': '{{$c->name}} <br> {{$c->chat_id}}'
                     },
                     @endforeach
                 ]
