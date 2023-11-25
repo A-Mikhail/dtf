43130fd3037ae4293b86b843a1460663199aea0f
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-25 23:43:23 +0600

    php eol php method

--- application/views/dtf/kanban.blade.php
+++ application/views/dtf/kanban.blade.php
@@ -31,7 +31,7 @@
                 'item': [
                     @foreach($clients as $c)
                     {
-                        'title': '{{$c->name PHP_EOL $c->chat_id}}'
+                        'title': '<?php $c->name . PHP_EOL . $c->chat_id}} ?>'
                     },
                     @endforeach
                 ]
