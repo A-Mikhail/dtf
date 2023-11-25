d982a284c4adf93bb6a1ebca2d83ce4f18b17898
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-11-25 22:07:25 +0600

    test

--- application/controllers/webhooks.php
+++ application/controllers/webhooks.php
@@ -97,6 +97,8 @@ class Webhooks_Controller extends Base_Controller {
     private function create_client ($data) {
         $client = Client::where('chat_id', '=', $data->chatId)->get();
         
+        var_dump($client);
+        
         if (!is_null($client)) {
             $client = new Client();
     
