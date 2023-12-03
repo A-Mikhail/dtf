5fb078e820e477c475e7c7114bc1e9c30f1fbd72
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-12-03 11:13:03 +0600

    fill log

--- application/views/dtf/client.blade.php
+++ application/views/dtf/client.blade.php
@@ -22,17 +22,21 @@
             @if($clientLog)
             <div class="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
                 <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-                        <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32"
-                            class="rounded-circle flex-shrink-0">
-                        <div class="d-flex gap-2 w-100 justify-content-between">
-                            <div>
-                                <h6 class="mb-0">List group item heading</h6>
-                                <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
+                    @foreach($clientLog as $cl)
+                        <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                            <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32"
+                                class="rounded-circle flex-shrink-0">
+
+                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                <div>
+                                    <h6 class="mb-0">{{$cl->type->rustatus()}}</h6>
+                                    <p class="mb-0 opacity-75">{{$cl->comment}}</p>
+                                    <p class="mb-0 opacity-75">{{$cl->author}}</p>
+                                </div>
+                                <small class="opacity-50 text-nowrap">{{$cl->created_at->format('d-m-Y')}}</small>
                             </div>
-                            <small class="opacity-50 text-nowrap">now</small>
-                        </div>
-                    </a>
+                        </a>
+                    @endforeach
                 </div>
             </div>
             @else
