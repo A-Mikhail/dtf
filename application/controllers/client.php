f38f4e0eb12b11bd2be419dd0ff9aae98af592ec
A-Mikhail <mikhail.adamenko@protonmail.com>
2023-12-12 21:04:15 +0600

    remove chatType

--- application/controllers/client.php
+++ application/controllers/client.php
@@ -24,7 +24,6 @@ class Client_Controller extends Base_Controller {
                 ),
                 "scope" => "card",
                 "filter" => array(array(
-                    "chatType" => "whatsapp",
                     "chatId" => $client->chat_id,
                     "name" => $client->name
                 )),
