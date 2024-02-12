8e99f3892a0147422def09513174890d4f965cb5
A-Mikhail <mikhail.adamenko@protonmail.com>
2024-02-12 12:47:02 +0600

    ui

--- application/views/dtf/register.blade.php
+++ application/views/dtf/register.blade.php
@@ -10,32 +10,34 @@
         <div class="col-8">
             <h1 class="h3 mb-3 fw-normal">Регистрация пользователя</h1>
     
+            <label for="fio" class="form-label">ФИО</label>
             <div class="input-group mb-3">
-                <label for="fio" class="form-label">ФИО</label>
                 <input type="text" class="form-control" name="fio" id="fio" placeholder="ФИО">
             </div>
     
+            <label for="login" class="form-label">Логин</label>
             <div class="input-group mb-3">
-                <label for="login" class="form-label">Логин</label>
                 <input type="text" class="form-control" name="login" id="floatingInput" placeholder="Логин">
             </div>
     
+            <label for="password" class="form-label">Пароль</label>
             <div class="input-group mb-3">
-                <label for="password" class="form-label">Пароль</label>
                 <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Пароль">
             </div>
     
+            <label for="repeatpassword" class="form-label">Повторите пароль</label>
             <div class="input-group mb-3">
-                <label for="repeatpassword" class="form-label">Повторите пароль</label>
                 <input type="password" class="form-control" name="repeatpassword" id="floatingPassword"
                     placeholder="Повторите пароль">
             </div>
     
-            <select class="form-control" name="alevel" data-placeholder="Выберите уровень доступа">
-                <option value="">Выберите уровень доступа</option>
-                <option value="1">Администратор</option>
-                <option value="2">Менеджер</option>
-            </select>
+            <div class="input-group mb-3">
+                <select class="form-control" name="alevel" data-placeholder="Выберите уровень доступа">
+                    <option value="">Выберите уровень доступа</option>
+                    <option value="1">Администратор</option>
+                    <option value="2">Менеджер</option>
+                </select>
+            </div>
     
             <button class="btn btn-primary w-100 py-2 btn-create-user" type="submit">Создать пользователя</button>
         </div>
