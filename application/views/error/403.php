c5130957a9604cea2ccc6476dc1bb7eb93a9b8bd
A-Mikhail <mikhail.adamenko@protonmail.com>
2024-02-12 13:51:54 +0600

    404 error

--- application/views/error/403.php
+++ application/views/error/403.php
@@ -25,7 +25,8 @@
     <div class="container py-3">
         <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
             <h1 class="display-4 fw-normal text-body-emphasis">403</h1>
-            <p class="fs-5 text-body-secondary">Ошибка 403 - Доступ запрещён. Пожалуйста, вернитесь на предыдущую страницу или запросите доступ к этой странице у администратора.</p>
+            <p class="fs-5 text-body-secondary">Ошибка 403 - Доступ запрещён.</p> 
+            <p class="fs-5 text-body-secondary">Пожалуйста, вернитесь на предыдущую страницу или запросите доступ к этой странице у администратора.</p>
         </div>
     </div>
 </body>
--- application/views/error/404.php
+++ application/views/error/404.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
+
 <head>
-    <meta charset="UTF-8">
-    <title>404</title>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>DTF.KZ - 403</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" id="badge-icon">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel='stylesheet' type='text/css' media='screen' href='/libs/bootstrap/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="/libs/fontawesome/all.min.css">
+    <link rel="stylesheet" href="/libs/mdb/css/mdb.min.css">
+
+    <link rel='stylesheet' type='text/css' media='screen' href='/main.css?v=0.0.2'>
 </head>
+
 <body>
-    404
+    <div class="container py-3">
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 fw-normal text-body-emphasis">403</h1>
+            <p class="fs-5 text-body-secondary">Ошибка 404 - Не найдена страница.</p> 
+            <p class="fs-5 text-body-secondary">Страница которую вы искали не существует 😥, провьрте правильность ввода и повторите вновь.</p>
+        </div>
+    </div>
 </body>
+
 </html>
