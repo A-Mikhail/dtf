c4c569dbf7616f30c5e0e17a132887e0e9481cae
A-Mikhail <mikhail.adamenko@protonmail.com>
2024-02-13 12:12:15 +0600

    div row

--- application/views/dtf/dashboard/tasks.blade.php
+++ application/views/dtf/dashboard/tasks.blade.php
@@ -6,49 +6,53 @@
 
 @section('content')
 <div class="container px-4 py-5">
-    <!-- Статистика за сегодня -->
-    <h4 class="pb-2 border-bottom">Статистика за сегодня</h4>
+    <div class="row">
+        <!-- Статистика за сегодня -->
+        <h4 class="pb-2 border-bottom">Статистика за сегодня</h4>
 
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
-        @foreach($statbynow as $sn)
-        <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#bootstrap" />
-            </svg>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+            @foreach($statbynow as $sn)
+            <div class="col d-flex align-items-start">
+                <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <use xlink:href="#bootstrap" />
+                </svg>
 
-            <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">{{$sn->cnt}}</h3>
-                <p>{{__("statuses.$sn->current_status")}}</p>
+                <div>
+                    <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">{{$sn->cnt}}</h3>
+                    <p>{{__("statuses.$sn->current_status")}}</p>
+                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
     
-    <!-- Статистика за период -->
-    <h4 class="pb-2 border-bottom">Статистика c {{date_format(date_create($from), 'd.m.Y')}} по {{date_format(date_create($to), 'd.m.Y')}}</h4>
-
-    <table class="table align-middle mb-0 bg-white">
-        <thead class="bg-light">
-            <tr>
-                <th>Статус</th>
-                <th>Кол-во</th>
-                <th>Дата</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($statbydate as $sd)
-            <tr>
-                <td>
-                    <span class="badge badge-success rounded-pill d-inline">{{__("statuses.$sd->current_status")}}</span>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">{{$sd->cnt}}</p>
-                </td>
-                <td>{{date_format(date_create($sd->times), 'd.m.Y')}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="row">
+        <!-- Статистика за период -->
+        <h4 class="pb-2 border-bottom">Статистика c {{date_format(date_create($from), 'd.m.Y')}} по {{date_format(date_create($to), 'd.m.Y')}}</h4>
+    
+        <table class="table align-middle mb-0 bg-white">
+            <thead class="bg-light">
+                <tr>
+                    <th>Статус</th>
+                    <th>Кол-во</th>
+                    <th>Дата</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($statbydate as $sd)
+                <tr>
+                    <td>
+                        <span class="badge badge-success rounded-pill d-inline">{{__("statuses.$sd->current_status")}}</span>
+                    </td>
+                    <td>
+                        <p class="fw-normal mb-1">{{$sd->cnt}}</p>
+                    </td>
+                    <td>{{date_format(date_create($sd->times), 'd.m.Y')}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
