8cb843d40cb3577c0c73bdbeff52f6c234bdc4af
A-Mikhail <mikhail.adamenko@protonmail.com>
2024-02-14 17:28:53 +0600

    comma

--- application/controllers/table.php
+++ application/controllers/table.php
@@ -29,7 +29,7 @@ class Table_Controller extends Base_Controller {
         // https://stackoverflow.com/questions/19763806/how-consider-null-as-the-max-date-instead-of-ignoring-it-in-mysql
         // ---------------------------------
         $clients = DB::query("select distinct clients.id, clients.name, clients.chat_id, clients.current_status, clients.updated_at, 
-            MAX(messages.date_time) AS new_update, case when MAX(deals.price <= 0) = 0 then MAX(deals.price) end AS last_price
+            MAX(messages.date_time) AS new_update, case when MAX(deals.price <= 0) = 0 then MAX(deals.price) end AS last_price,
             case when MAX(supplies.amount <= 0) = 0 then MAX(supplies.amount) end AS last_supply_m
             from clients 
             inner join messages on clients.chat_id = messages.chat_id
